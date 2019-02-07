aa596f9023330a60e3d8d9721a7a23a5d26e77e5
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-02-07 10:01:59 -0500

    everything is working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-// SEARCH
+// Allows the user to search on the search bar and when they hit search
+// it console.logs it, also replaces underscore with a space so its easier
+// for them to type
 document.querySelector("#park-btn").addEventListener("click", () =>{
     let parkInput = document.querySelector("#park-text").value;
     const newParkInput = parkInput.replace(" ", "_")
@@ -9,29 +11,16 @@ document.querySelector("#park-btn").addEventListener("click", () =>{
 // tolowercase
 
 
-// Search through the api
-// const searchParks = parkNameParam => {
-//     document.querySelector("#search-results").innerHTML = "";
-//     fetch(
-//       `https://paleobiodb.org/data1.2/taxa/list.json?park_name=${parkNameParam}`
-//     )
-//       .then(parks => parks.json())
-//       .then(parsedParks => {
-
-//       })
-
-
-
 const resultsString = (name, location) => {
    return `<li>${name}  ${location} <input type="submit" value="Save" id="save-btn" style="display:inline"/>
     </li>`
 }
 
-const franklin = "Hello my name is Franklin"
-const placeFranklin = "1234 happy street "
-document.querySelector("#search-results").innerHTML = resultsString(franklin)
+// const franklin = "Hello my name is Franklin"
+// const placeFranklin = "1234 happy street "
+// document.querySelector("#search-results").innerHTML = resultsString(franklin)
 
-console.log(resultsString(franklin, placeFranklin))
+// console.log(resultsString(franklin, placeFranklin))
 
 
 

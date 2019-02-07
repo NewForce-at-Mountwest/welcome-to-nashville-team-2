19176693ae6d74f5ac6752f19e2702d3abcca1c6
Megan <madden12@mctc.edu>
2019-02-07 13:44:39 -0500

    fixed search function

--- src/scripts/meetups.js
+++ src/scripts/meetups.js
@@ -1,22 +1,20 @@
 const searchMeetUpFunction = (searchMeetup) => {
-fetch("https://www.eventbriteapi.com/v3/events/search/?q=nashville&token=TJMRPXJLONUHWSFDGZJZ", {
-    headers: {
-        "Authorization": "Bearer TJMRPXJLONUHWSFDGZJZ",
-        "Accept": "application/json"
-    }
-})
-
-    .then(meetUps => meetUps.json())
-    .then(parsedMeetUps => {
-        console.log(parsedMeetUps);
-        let HTMLMeetString = "";
-        for (i = 0; i < 4; i++) {
-            // let urlInfo =" '<a href ='parsedMeetUps.events[i].url'>'MoreInfo'</a>'";
-            HTMLMeetString += resultsForMeetUpString(parsedMeetUps.events[i].name.text, parsedMeetUps.events[i].url)
-            
+    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_${searchMeetup}&token=TJMRPXJLONUHWSFDGZJZ`, {
+        headers: {
+            "Authorization": "Bearer TJMRPXJLONUHWSFDGZJZ",
+            "Accept": "application/json"
         }
-        document.querySelector("#search-results").innerHTML = HTMLMeetString;
     })
+
+        .then(meetUps => meetUps.json())
+        .then(parsedMeetUps => {
+            console.log(parsedMeetUps);
+            let HTMLMeetString = "";
+            for (i = 0; i < 4; i++) {
+                HTMLMeetString += resultsForMeetUpString(parsedMeetUps.events[i].name.text, parsedMeetUps.events[i].url)
+            }
+            document.querySelector("#search-results").innerHTML = HTMLMeetString;
+        })
 }
 document.querySelector("#concert-btn").addEventListener("click", () => {
     let userSearches = document.querySelector("#concert-text").value;

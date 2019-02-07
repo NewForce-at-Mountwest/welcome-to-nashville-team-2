2ebc8148e063981ce02231bce7ee790b1a6d5b2a
Megan <madden12@mctc.edu>
2019-02-07 15:11:29 -0500

    got search results to save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,8 +15,8 @@ const resultsString = (name, location, numerator, typeOfSearch) => {
    return `<li id=s${numerator + 1}>${name}  ${location} <input type="submit" value="Save" id="save-btn${numerator + 1}-${typeOfSearch}" style="display:inline"/>
     </li>`
 }
-const resultsForMeetUpString = (name, link) => {
-    return `<li>${name} <a href= "${link}"> More Info!</a> <input type="submit" value="Save" id="save-btn" style="display:inline"/>
+const resultsForMeetUpString = (name, link, numerator, typeOfSearch) => {
+    return `<li id=s${numerator + 1}>${name} <a href= "${link}"> More Info!</a> <input type="submit" value="Save" id="save-btn${numerator + 1}-${typeOfSearch}" style="display:inline"/>
      </li>`
  }
 
@@ -69,11 +69,3 @@ document.querySelector(".results-container").addEventListener("click", () => {
 // var elmnt = document.getElementsByTagName("DIV")[0];
 // var cln = elmnt.cloneNode(true);
 // document.body.appendChild(cln);
-
-
-
-
-
-
-
-
--- src/scripts/meetups.js
+++ src/scripts/meetups.js
@@ -11,7 +11,7 @@ const searchMeetUpFunction = (searchMeetup) => {
             console.log(parsedMeetUps);
             let HTMLMeetString = "";
             for (i = 0; i < 4; i++) {
-                HTMLMeetString += resultsForMeetUpString(parsedMeetUps.events[i].name.text, parsedMeetUps.events[i].url)
+                HTMLMeetString += resultsForMeetUpString(parsedMeetUps.events[i].name.text, parsedMeetUps.events[i].url, i, "event")
             }
             document.querySelector("#search-results").innerHTML = HTMLMeetString;
         })
@@ -21,7 +21,46 @@ document.querySelector("#concert-btn").addEventListener("click", () => {
     searchMeetUpFunction(userSearches);
     console.log(userSearches);
 })
+// **********for the events saved searches**********
+document.querySelector(".results-container").addEventListener("click", () => {
+    if(event.target === document.querySelector("#save-btn1-event")) {
+    let save1 = document.querySelector("#s1").textContent
+    // const cln = save1.cloneNode(true)
+    // const cln1 = cln.remove("input")
+
+    document.querySelector("#concert-save").innerHTML = `Event: ${save1}`
+    // document.querySelector("#park-save").insertAdjacentHTML(save1)
+
+    // console.log("you clicked me")
+    // console.log(save1)
+    } else if (event.target === document.querySelector("#save-btn2-event")) {
+        let save2 = document.querySelector("#s2").textContent
+
+
+        document.querySelector("#concert-save").innerHTML = `Event: ${save2}`
+
+        } else if (event.target === document.querySelector("#save-btn3-event")) {
+            let save3 = document.querySelector("#s3").textContent
+
+
+            document.querySelector("#concert-save").innerHTML = `Event: ${save3}`
+
+            // console.log("you clicked me")
+            } else if (event.target === document.querySelector("#save-btn4-event")) {
+                let save4 = document.querySelector("#s4").textContent
+
+                document.querySelector("#concert-save").innerHTML = `Event: ${save4}`}
+
+
+
+            })
 
 
 
 
+
+
+
+
+
+           

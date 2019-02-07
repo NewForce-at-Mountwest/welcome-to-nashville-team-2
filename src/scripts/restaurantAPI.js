74219e2c2313277f7f9c8248274e2112d5ae089f
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-02-07 14:29:47 -0500

    merging

--- src/scripts/restaurantAPI.js
+++ src/scripts/restaurantAPI.js
@@ -6,30 +6,32 @@
 // Your API key must be added as user-key to the headers configuration for your request
 // API # a9221722337b768a723cd53c1409cb80
 const restaruantAPI = (inquiry) => {
-    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${inquiry}`, {
-        headers: {
-            "Accept": "application/json",
-            "user-key": "a9221722337b768a723cd53c1409cb80"
-        }
-    })
+    // fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&q=${inquiry}`, {
+    //     headers: {
+    //         "Accept": "application/json",
+    //         "user-key": "a9221722337b768a723cd53c1409cb80"
+    //     }
+    // })
+    console.log("hi")
+    fetch("http://localhost:8088/restaurants")
     .then(r => r.json())
     .then(results => {
         let htmlString =""
+        console.log(results)
         for(let i=0; i < 4; i++) {
-            htmlString += resultsString(results.restaurants[i].restaurant.name, results.restaurants[i].restaurant.location.address)
+            htmlString += resultsString(results[i].restaurant.name, results[i].restaurant.location.address)
 
         }
         document.querySelector("#search-results").innerHTML = htmlString
     })
 
 
+
+}
 document.querySelector("#restaurant-btn").addEventListener("click", () => {
     let userSearch = document.querySelector("#restaurant-text").value
     restaruantAPI(userSearch)
 
 })
-
-
-}
 
 // restaruantAPI("cookies")

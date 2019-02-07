9c412168ae9160e9704982e0acb89111ebb5db29
Megan <madden12@mctc.edu>
2019-02-07 13:05:26 -0500

    got search for events to populate results

--- src/scripts/meetups.js
+++ src/scripts/meetups.js
@@ -1,25 +1,29 @@
 const searchMeetUpFunction = (searchMeetup) => {
-    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_${searchMeetup}&token=TJMRPXJLONUHWSFDGZJZ`, {
-        headers: {
-            "Authorization": "Bearer TJMRPXJLONUHWSFDGZJZ",
-            "Accept": "application/json"
-        }
-    })
-        .then(meetUps => meetUps.json())
-        .then(parsedMeetUps => {
-            // console.log(parsedMeetUps.events[0]);
-            let HTMLMeetString = "";
-            for (i = 0; i < 4; i++) {
-              HTMLMeetString += meetUpString(parsedMeetUps.event[i].name.text)
-            }
-                document.querySelector("#search-results").innerHTML = HTMLMeetString
-            })
+fetch("https://www.eventbriteapi.com/v3/events/search/?q=nashville&token=TJMRPXJLONUHWSFDGZJZ", {
+    headers: {
+        "Authorization": "Bearer TJMRPXJLONUHWSFDGZJZ",
+        "Accept": "application/json"
+    }
+})
+
+    .then(meetUps => meetUps.json())
+    .then(parsedMeetUps => {
+        console.log(parsedMeetUps);
+        let HTMLMeetString = "";
+        for (i = 0; i < 4; i++) {
+            // let urlInfo =" '<a href ='parsedMeetUps.events[i].url'>'MoreInfo'</a>'";
+            HTMLMeetString += resultsForMeetUpString(parsedMeetUps.events[i].name.text, parsedMeetUps.events[i].url)
             
-            document.querySelector("#concert-btn").addEventListener("click", () => {
-                let userSearches = document.querySelector("#concert-text").value;
-            })
         }
-        searchMeetUpFunction(userSearches);
+        document.querySelector("#search-results").innerHTML = HTMLMeetString;
+    })
+}
+document.querySelector("#concert-btn").addEventListener("click", () => {
+    let userSearches = document.querySelector("#concert-text").value;
+    searchMeetUpFunction(userSearches);
+    console.log(userSearches);
+})
 
 
-console.log(searchMeetUpFunction);
+
+

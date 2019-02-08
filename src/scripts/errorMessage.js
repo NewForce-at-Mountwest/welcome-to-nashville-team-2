2c9cacdc3d6bb749af08d0378ff811d9ea71b913
Megan <madden12@mctc.edu>
2019-02-08 13:54:04 -0500

    error message

--- src/scripts/errorMessage.js
+++ src/scripts/errorMessage.js
@@ -4,36 +4,36 @@ const handleWeirdSearchInputPark = (warningMessagePark) => {
     document.querySelector(
       "#search-results"
     ).innerHTML = `<div class="jumbotron">
-  <h1 class="display-4">Not Found</h1>
-  <p class="lead">${warningMessagePark}</p>
+  <h1 class="display-4">Error</h1>
+  <p class="lead">"Search Term Not Found!</p>
   <hr class="my-4">
   <p>Please try something else, like "baseball field" or "dog park"</p>
 </div>`;
+    }
 
-}
-// function that just prints error messages for Restaurants
-const handleWeirdSearchInputFood = (warningMessageFood) => {
-    // if it doesn't have a .records property, print an error message
-    document.querySelector(
-      "#search-results"
-    ).innerHTML = `<div class="jumbotron">
-  <h1 class="display-4">Not Found</h1>
-  <p class="lead">${warningMessageFood}</p>
-  <hr class="my-4">
-  <p>Please try something else, like "burgers" or "pizza"</p>
-</div>`;
 
-}
+// // function that just prints error messages for Restaurants
+// const handleWeirdSearchInputFood = (warningMessageFood) => {
+//     // if it doesn't have a .records property, print an error message
+//     document.querySelector(
+//       "#search-results"
+//     ).innerHTML = `<div class="jumbotron">
+//   <h1 class="display-4">Not Found</h1>
+//   <p class="lead">${warningMessageFood}</p>
+//   <hr class="my-4">
+//   <p>Please try something else, like "burger"</p>
+//   </div>`;
+
+// }
 // function that just prints error messages for Events
 const handleWeirdSearchInputEvent = (warningMessageEvent) => {
     // if it doesn't have a .records property, print an error message
     document.querySelector(
       "#search-results"
     ).innerHTML = `<div class="jumbotron">
-  <h1 class="display-4">Not Found</h1>
-  <p class="lead">${warningMessageEvent}</p>
+  <h1 class="display-4">Error!</h1>
+  <p class="lead">"Search Term Not Found"</p>
   <hr class="my-4">
   <p>Please try something else, like "beachbody" or "art"</p>
 </div>`;
-
-}
+    }
